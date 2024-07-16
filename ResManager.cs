1d6df0e6d39045333636074efb2194a8ce857fe9
jramstedt <janne.ramstedt@aniway.fi>
2024-07-16 19:04:59 +0300

    Fixed compound resource size calculation. Check if there is old chunk entry data. Show full path on warning console messages.

--- ResManager.cs
+++ ResManager.cs
@@ -29,8 +29,8 @@ public static class ResManager {
         var resFile = new ResourceFile(rawBytes);
         
         foreach (var (resId, resource) in resFile.ResourceEntries) {
-            if (resId == 0) {
-                Console.WriteLine($"Warning ({(global::System.IO.Path.GetFileName(filePath))}): Skipping resource with id 0.");
+            if (resId < 3) {
+                Console.WriteLine($"Warning ({filePath}): Skipping resource with id {resId}.");
                 continue;
             }
 
@@ -38,13 +38,13 @@ public static class ResManager {
             var flags = resource.info.Flags;
 
             if (resource.info.Flags.HasFlag(ResourceFile.ResourceFlags.LZW)) {
-                Console.WriteLine($"Warning ({(global::System.IO.Path.GetFileName(filePath))}/{resId:X4}): LZW Packing unsupported. Will write output as unpacked.");
+                Console.WriteLine($"Warning ({filePath}/{resId:X4}): LZW Packing unsupported. Will write output as unpacked.");
                 flags &= ~ResourceFile.ResourceFlags.LZW;
             }
 
             if (resourceRecord.TryGetValue(resId, out var prevResource)) {
                 if (resource.info.ContentType != prevResource.Info.ContentType)
-                    Console.WriteLine($"Warning ({(global::System.IO.Path.GetFileName(filePath))}/{resId:X4}): Content types do not match old: {prevResource.Info.ContentType} new: {resource.info.ContentType }. Will use new content type.");
+                    Console.WriteLine($"Warning ({filePath}/{resId:X4}): Content types do not match old: {prevResource.Info.ContentType} new: {resource.info.ContentType }. Will use new content type.");
                 
                 /*
                  * Merge
@@ -52,20 +52,28 @@ public static class ResManager {
                  */
                 for (var chunkEntryIndex = 0; chunkEntryIndex < chunkData.Length; ++chunkEntryIndex) {
                     var newEntryData = chunkData[chunkEntryIndex];
-                    var prevEntryData = prevResource.ChunkData[chunkEntryIndex];
 
                     if (newEntryData.Length == 0) { // Don't override
-                        chunkData[chunkEntryIndex] = prevEntryData; // Use old data
+                        if (chunkEntryIndex < prevResource.ChunkData.Length)
+                            chunkData[chunkEntryIndex] = prevResource.ChunkData[chunkEntryIndex]; // Use old data
+                        
                         continue;
                     }
 
-                    Console.WriteLine($"{(global::System.IO.Path.GetFileName(filePath))}: Patching {resId:X4}/{chunkEntryIndex} type: {resource.info.ContentType}");
+                    Console.WriteLine($"{filePath}: Patching {resId:X4}/{chunkEntryIndex} type: {resource.info.ContentType}");
                 }
             } else {
-                Console.WriteLine($"{(global::System.IO.Path.GetFileName(filePath))}: Adding {resId:X4} type: {resource.info.ContentType}");
+                Console.WriteLine($"{filePath}: Adding {resId:X4} type: {resource.info.ContentType}");
             }
             
             var totalLength = chunkData.Aggregate(0, (total, entryData) => total + entryData.Length);
+            if (resource.info.Flags.HasFlag(ResourceFile.ResourceFlags.Compound)) {
+                // TODO FIXME Calculates header size here. This should be handled when Compound header is written...
+                
+                ushort blockCount = (ushort)chunkData.Length;
+                int headerSize = sizeof(ushort) + (blockCount + 1) * sizeof(int);
+                totalLength += headerSize;
+            }
 
             resourceRecord[resId] = new ResourceData() {
                 Info = new ResourceFile.DirectoryEntry() {
